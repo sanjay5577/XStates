845553205da16c5bbb9ee401984f9ffcac34d94d
sanjay5577 <143835637+sanjay5577@users.noreply.github.com>
2024-01-27 15:57:59 +0000

    final

--- states/src/App.js
+++ states/src/App.js
@@ -6,42 +6,145 @@ function App() {
 
   const[select , setSelected] = useState({
     selectcountry : "Select Country",
-    selectstate : "Select state",
-    selectcity : "select City"
+    selectstate : "Select State",
+    selectcity : "Select City"
 
   });
   const[countrylist ,setCountryList] = useState([]);
   const[statelist ,setStateList] = useState([]);
   const[citylist ,setCityList] = useState([]);
 
-  const fetchcountrylist=()=>{
-    axios.get(`https://crio-location-selector.onrender.com/countries`)
-    .then((response )=>{
-      setCountryList(response);
+  const fetchlist= async(endpointurl)=>{
+
+    try{
+      console.log(endpointurl);
+      const apidata = await axios.get(`${endpointurl}`);
+      return apidata.data;
+
+    }catch(e){
+      console.log(e.message)
+    }
+  }
+
+  useEffect(()=>{
+    fetchlist("https://crio-location-selector.onrender.com/countries")
+    .then((countrylist)=>{
+      setCountryList(countrylist);
     })
     .catch((e)=>{
       console.log(e.message)
     })
-  }
+  },[]);
 
   useEffect(()=>{
-     fetchcountrylist();
-  },[])
+
+    if(select.selectcountry !=="Select Country"){
+      
+
+
+      fetchlist(`https://crio-location-selector.onrender.com/country=${select.selectcountry}/states`)
+    .then((statelist)=>{
+      console.log(statelist);
+      setStateList(statelist);
+    })
+    .catch((e)=>{
+      console.log(e.message)
+    })
+
+    }
+
+    if(select.selectcountry ==="Select Country"){
+      setStateList([]);
+      select.selectstate ="Select State"
+    }
+
+    if(select.selectstate !=="Select State" && select.selectstate !=="N/A"){
+      fetchlist(`https://crio-location-selector.onrender.com/country=${select.selectcountry}/state=${select.selectstate}/cities`)
+    .then((citylist)=>{
+      console.log(citylist);
+      setCityList(citylist);
+    })
+    .catch((e)=>{
+      console.log(e.message)
+    })
+
+    }
+
+    if(select.selectstate ==="Select State"){
+      setCityList([]);
+      select.selectcity ="Select City"
+    }
+
+  },[select.selectcountry ,  select.selectstate]);
+
+
+
+  const handleselect =(event)=>{
+    const name = event.target.name;
+    const value = event.target.value;
+
+    if(name ==="selectcountry"){
+      setStateList([]);
+      select.selectstate ="Select State"
+    }
+    setSelected((values)=> ({...values, [name]:value}));
+  }
+
+
+
+
   return (
     <div className="App">
       <h1>Select Location</h1>
       <select className ="countrydropdown"
             disabled={false}
+            name="selectcountry"
+            value={select.selectcountry}
+            onChange={handleselect}
             
         >
           <option>Select Country</option>
-          {countrylist.map((item) => (
+          {countrylist.length !==0 && countrylist.map((item) => (
             <option >
                 {item}
             </option>
             ))}
             
         </select>
+
+        <select className ="dropdown"
+            disabled={select.selectcountry !=="Select Country" ? false:true}
+            name="selectstate"
+            value={select.selectstate}
+            onChange={handleselect}
+            
+        >
+          <option>Select State</option>
+          {statelist.length !==0 && statelist.map((item) => (
+            <option >
+                {item}
+            </option>
+            ))}
+            
+        </select>
+
+        <select className ="dropdown"
+            disabled={select.selectstate !=="Select State" ? false:true}
+            name="selectcity"
+            value={select.selectcity}
+            onChange={handleselect}
+            
+        >
+          <option>Select City</option>
+          {citylist.length !==0 && citylist.map((item) => (
+            <option >
+                {item}
+            </option>
+            ))}
+            
+        </select>
+
+        {select.selectcity !=="Select City" &&<h4>You Selected <span className="city">{select.selectcity}</span>, <span className="statecountry">{select.selectstate}, {select.selectcountry}</span></h4>}
     </div>
   );
 }
